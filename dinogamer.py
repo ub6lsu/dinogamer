c36dd9b56ca8a79ea5c6542730ed0050d2808cf8
ub6lsu <84380182+ub6lsu@users.noreply.github.com>
2024-12-27 17:46:20 +0300

    Update dinogamer.py

--- dinogamer.py
+++ dinogamer.py
@@ -1,12 +1,16 @@
 import pyautogui
 le=10
-xk=460
+xk=465
 yk=240
 while True: 
  pic = pyautogui.screenshot(region=(xk,yk,le,1))
  jmp = False
  for i in range(0,le):
-  jmp = jmp or (pic.getpixel((i,0)) == (172,172,172))
+  pix=pic.getpixel((i,0))
+  #print(pix)
+  #jmp = jmp or (pix == (172,172,172))
+  jmp = jmp or (pix != (32,33,36))
  if jmp: 
+  print(pix)
   pyautogui.press("space")
- #print (pix)
+
