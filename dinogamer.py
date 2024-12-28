83b9d62854ee8b1a93b6eedcfa75d4e052ef9530
ub6lsu <84380182+ub6lsu@users.noreply.github.com>
2024-12-28 11:36:32 +0300

    Update dinogamer.py

--- dinogamer.py
+++ dinogamer.py
@@ -1,16 +1,27 @@
 import pyautogui
-le=10
-xk=465
-yk=240
+import time
+le = 30
+xk = 480
+yk = 240
+xp = xk
+yp = 213
+
 while True: 
- pic = pyautogui.screenshot(region=(xk,yk,le,1))
+ cactus = pyautogui.screenshot(region=(xk,yk,le,1))
  jmp = False
  for i in range(0,le):
-  pix=pic.getpixel((i,0))
-  #print(pix)
-  #jmp = jmp or (pix == (172,172,172))
-  jmp = jmp or (pix != (32,33,36))
+  pix=cactus.getpixel((i,0))
+  if (jmp := jmp or (pix == (172,172,172))): break
  if jmp: 
-  print(pix)
-  pyautogui.press("space")
-
+  pyautogui.keyDown("space")
+  pyautogui.keyDown("space")
+  pyautogui.keyUp("space")
+ ptero = pyautogui.screenshot(region=(xp,yp,le,1))
+ sit = False
+ for i in range(0,le):
+  pix=ptero.getpixel((i,0))
+  if (sit := sit or (pix == (172,172,172))): break
+ if sit: 
+  pyautogui.keyDown("down")
+  time.sleep(0.5)
+  pyautogui.keyUp("down")
